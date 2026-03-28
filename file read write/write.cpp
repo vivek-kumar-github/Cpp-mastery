fb3e30ee5729c25748a0306365869b9ef8833001
Vivek Kumar <viveksingh599999@gmail.com>
2026-03-28 18:48:31 +0530

    Initial commit: Basic C++ setup and first programs

---
+++ file read write/write.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+int main() {
+    std::string filename = "file.txt";
+    std::string content = "Hello! from file.txt";
+    std::ofstream file(filename);
+    if (!file) {
+        std::cout << "Error during opening file" << std::endl;
+        return 1;
+    }
+    file << content;
+    std::cout << "Content successfully writen in file" << std::endl;
+    return 0;
+}
---
+++ hello world/hello.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello! Vivek" << std::endl;
+    return 0;
+}
