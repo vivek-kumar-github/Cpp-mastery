cbe395cc6f4788deac49cf774d4ce75df83ef48f
Vivek Kumar <viveksingh599999@gmail.com>
2026-05-01 07:09:37 +0530

    chore: add pointers

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -21,6 +21,25 @@ int main() {
 
     //pointer must be of the same data type as the object it points to.
 
+    //the size of a pointer is always fixed (since you only store the address and nothing more).
+    //You can have an object of any size somewhere, even 1GB, without passing it around in our program,
+    // and always manipulate a pointer to its memory address
+    double pi = 3.14;
+    double *piptr = &pi;
+    cout << "Size of double pointer " << sizeof(piptr) << endl;
+
+    //When you dereference a pointer, it means getting the value that is stored at the memory address it points to.
+    // You do it using the dereference operator (*). Dereferencing allows you to read or modify the value at that memory location:
+    cout << "Value of piptr " << *piptr << endl;
+
+    //A pointer only declared and not initialized is called null pointer and is dangerous because it points to any unknown
+    //memory location and dereferencing and modifying it can break any program
+    //e.g. int *ptr; *ptr = 100;
+
+    //To avoid this we use null pointer A null pointer is a special value that indicates it is not pointing to any valid memory address.
+    // We can assign it explicitly using the value (nullptr) or by assigning a zero to the pointer.
+    int *ptr1 = nullptr;
+    int *ptr2 = 0; //this is not recommended
     
     return 0;
 }
